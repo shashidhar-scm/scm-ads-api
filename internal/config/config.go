9855413070fd8d1e70d1476df82cb4693f8aef6d
shashidhar <shashidhar@uttungaa.com>
2025-12-12 12:49:19 +0530

    psql config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,10 @@
 // internal/config/config.go
 package config
 
-import "os"
+import (
+	"net/url"
+	"os"
+)
 
 type Config struct {
 	Port        string
@@ -10,10 +13,30 @@ type Config struct {
 }
 
 func Load() *Config {
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		host := getEnv("PSQL_HOST", "localhost")
+		port := getEnv("PSQL_PORT", "5432")
+		user := getEnv("PSQL_USER", "postgres")
+		password := getEnv("PSQL_PASSWORD", "Asterisk@123")
+		dbName := getEnv("PSQL_DB_NAME", "scm_ads")
+
+		u := &url.URL{
+			Scheme: "postgres",
+			User:   url.UserPassword(user, password),
+			Host:   host + ":" + port,
+			Path:   dbName,
+		}
+		q := u.Query()
+		q.Set("sslmode", "disable")
+		u.RawQuery = q.Encode()
+		databaseURL = u.String()
+	}
+
 	return &Config{
 		Port:        getEnv("PORT", "8080"),
 		Environment: getEnv("ENVIRONMENT", "development"),
-		DatabaseURL: getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/scm_ads?sslmode=disable"),
+		DatabaseURL: databaseURL,
 	}
 }
 
